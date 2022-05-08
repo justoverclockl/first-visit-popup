c11f27253e88f9184aef5759e7949a3b9da321df
Marco Colia <info@justoverclock.it>
2022-05-09 01:07:19 +0200

    remove sanitising

--- js/src/forum/components/FirstVisitPopup.js
+++ js/src/forum/components/FirstVisitPopup.js
@@ -1,6 +1,5 @@
 import Modal from 'flarum/common/components/Modal';
 import app from 'flarum/forum/app';
-import DOMPurify from 'dompurify';
 
 export default class CustomModal extends Modal {
   static isDismissible = true;
@@ -9,22 +8,7 @@ export default class CustomModal extends Modal {
     super.oncreate(vnode);
     const modalContent = app.forum.attribute('justoverclock-first-visit-popup.modalContent');
     const modal = document.getElementById('fvp-modal');
-    modal.innerHTML = DOMPurify.sanitize(modalContent, {
-      USE_PROFILES: { html: true },
-      FORCE_BODY: true,
-      ALLOWED_ATTR: ['style', 'class', 'type', 'href', 'rel'],
-      ALLOWED_TAGS: [
-        'link',
-        'figure',
-        'table',
-        'caption',
-        'thead',
-        'tr',
-        'th',
-        'tbody',
-        'td',
-      ],
-    });
+    modal.innerHTML = modalContent
   }
 
   className() {
